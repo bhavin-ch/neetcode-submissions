e4d499b3ec092102bad9a2bbc597646a1a1a4c2a
Bhavin <bhavin137@gmail.com>
2026-04-27 14:54:11 +0200

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let max_profit = 0
+        const n = prices.length
+        for (let i=0; i < n-1; i++) {
+            const max_price = Math.max(...prices.slice(i, n))
+            if (max_price > prices[i]) {
+                const profit = max_price - prices[i]
+                max_profit = (profit > max_profit) ? profit : max_profit
+            }
+        }
+        return max_profit
+    }
+    // maxProfit(prices) {
+    //     let max_profit = 0
+    //     let min_price = prices[0]
+    //     for (let i=1; i < prices.length; i++) {
+    //         let profit = prices[i] - min_price
+    //         max_profit = Math.max(profit, max_profit)
+    //         min_price = Math.max(min_price, prices[i])
+    //     }
+    //     return max_profit
+    // }
+}
